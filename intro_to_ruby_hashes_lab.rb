4612f0b1820dca153b9bdaf19cf53db35e7bf290
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 21:36:57 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,22 +43,7 @@ monopoly =
     },
     
     :names =>
-    {
-      :reading_railroad => {"mortgage_value" => "$100"},
-      
-      :pennsylvania_railroad =>
-      {
-        "mortgage_value" => "$200"
-      },
-      :b_and_o_railroad =>
-      {
-        "mortgage_value" => "$400"
-      },
-      :shortline =>
-      {
-        "mortgage_value" => "$800"
-      }
-    }
+    {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad =>{"mortgage_value" => "$400"},:shortline => {"mortgage_value" => "$800"}}
   }
 }
 	
