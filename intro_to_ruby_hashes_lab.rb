56a45df1c8f062a6c8368ddac9564e356ee08201
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 21:32:10 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_fourth_tier
 	
 	{pieces: 4,
 	
-		:rent_in_dollars => 
+	:rent_in_dollars => 
 	  {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned => 200},
 	
 	:names =>
