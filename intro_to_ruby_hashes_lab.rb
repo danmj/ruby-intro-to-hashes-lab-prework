32c1c910b2b495923a60bdaccaadd25bfed65a52
danmj <daniel.jung@stern.nyu.edu>
2018-08-17 21:17:41 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,8 +18,7 @@ end
 def monopoly_with_third_tier
 		monopoly = {:railroads => {pieces: 4, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}},
 		
-		:rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned => 200} 
-		  
+		  :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned => 200} 
 		  }
 		}
 		
@@ -32,11 +31,13 @@ def monopoly_with_fourth_tier
 	
 	{pieces: 4,
 	
+		:names =>
+	{:reading_railroad => {"mortgage_value": "$10"}, :pennsylvania_railroad => {"mortgage_value": "$20"}, :b_and_o_railroad => {"mortgage_value": "$40"}, :shortline => {"mortgage_value": "$80"}},
+	
 	:rent_in_dollars => 
 	  {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned => 200},
 	
-	:names =>
-	{:reading_railroad => {"mortgage_value": "$10"}, :pennsylvania_railroad => {"mortgage_value": "$20"}, :b_and_o_railroad => {"mortgage_value": "$40"}, :shortline => {"mortgage_value": "$80"}}
+
 	}
 	}
 	
